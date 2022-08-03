5ae71db53b6daf0cfe2018ed718785db911b9d67
Peter Zhou <peterzhou561@gmail.com>
2022-08-02 22:56:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql')
+var bodyParser = require('body-parser')
+var bcrypt = require('bcryptjs')
+var crypto = require('crypto')
+var fs = require('fs');
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  html = fs.readFileSync('index.html');
+  res.writeHead(200);
+  res.write(html);
+  res.end();
+});
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app
