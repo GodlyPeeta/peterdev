c5be10dc76277ad83e6b8c608344d3278183910d
GodlyPeeta <peterzhou561@gmail.com>
2025-03-03 04:59:40 -0500

    add listening 443

--- bin/www.js
+++ bin/www.js
@@ -3,21 +3,28 @@ let config = require('../config.json')
 let app = require('../app');
 let debug = require('debug')('peterdev:server');
 let http = require('http');
+let https = require('https');
 
 let argv = require('minimist')(process.argv.slice(2));
 
 let port = argv['p'];
 
-app.set('port', port);
+// app.set('port', port);
 
 /**
- * Create HTTP server.
+ * Create HTTP and HTTPS server.
  */
 
-let server = http.createServer(app);
+const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/privkey.pem')
+};
 
+let serverhttp = http.createServer(app);
+let serverhttps = https.createServer(options, app);
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+serverhttp.listen(80);
+serverhttps.listen(443);
