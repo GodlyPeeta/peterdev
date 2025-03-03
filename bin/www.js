4ce2a8d204d10dcee2eda590b24c549d5a95a9b1
GodlyPeeta <peterzhou561@gmail.com>
2025-03-03 05:16:04 -0500

    nvm

--- bin/www.js
+++ bin/www.js
@@ -5,26 +5,26 @@ let debug = require('debug')('peterdev:server');
 let http = require('http');
 let https = require('https');
 
+let fs = require("fs");
+
 let argv = require('minimist')(process.argv.slice(2));
 
 let port = argv['p'];
 
-// app.set('port', port);
+app.set('port', port);
 
 /**
  * Create HTTP and HTTPS server.
  */
 
-const options = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/privkey.pem')
-};
+// const options = {
+//     cert: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/fullchain.pem'),
+//     key: fs.readFileSync('/etc/letsencrypt/live/domain-or-subdomain.in/privkey.pem')
+// };
 
-let serverhttp = http.createServer(app);
-let serverhttps = https.createServer(options, app);
+let serverhttps = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-serverhttp.listen(80);
-serverhttps.listen(443);
+serverhttps.listen(port);
